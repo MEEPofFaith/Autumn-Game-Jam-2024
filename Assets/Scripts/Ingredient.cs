9e04e32293bd83af5fcad57d4257135f96f36129
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-11-23 11:10:57 -0800

    IngredientData

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour {
+    public IngredientData data;
+}
---
+++ Assets/Scripts/soscripts/IngredientData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient", menuName = "Ingredient", order = 1)]
+public class IngredientData : ScriptableObject{
+    public int smell;
+    public int flavor;
+    public int appearance;
+    public int texture; //mmmm cronchy
+
+    public string itemName = "Ingredient";
+    public string flavorText = "";
+
+    public Sprite image;
+}
