8643485c59ffb8060452ef28816e7c85d5a8b1ea
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-11-23 11:35:51 -0800

    Data go brrrr

--- Assets/Scripts/soscripts/CustomerData.cs
+++ Assets/Scripts/soscripts/CustomerData.cs
@@ -3,18 +3,17 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "CustomerData", menuName = "Customer", order = 0)]
 public class CustomerData : ScriptableObject {
-    public Species species;
-    public Type type;
+    public Species species = Species.raccoon;
+    public AnimalState type = AnimalState.normal;
     public string animalName;
     public string request;
-    public bool isAnts;
 
 
     public enum Species{
         raccoon, rat, opossum, squirrel, skunk, bird
     }
 
-    public enum Type{
+    public enum AnimalState{
         normal, ants, parasite
     }
 }
--- Assets/Scripts/soscripts/IngredientData.cs
+++ Assets/Scripts/soscripts/IngredientData.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "IngredientData", menuName = "Ingredient", order = 1)]
 public class IngredientData : ScriptableObject{
+    public string itemName = "Ingredient";
+    public string flavorText = "";
+
+    public IngredientType type = IngredientType.ingredient;
+
     public int smell;
     public int flavor;
     public int appearance;
     public int texture; //mmmm cronchy
 
-    public string itemName = "Ingredient";
-    public string flavorText = "";
-
     public Sprite sprite;
+
+    public enum IngredientType{
+        ingredient, seasoning
+    }
 }
